7f7dd335212c20c6c5ae95f22fc41e544ce5e453
Leon <Leon@DESKTOP-M4K0LFF>
2022-08-16 13:43:42 +0300

    2

---
+++ Lesson4Leon/CachedValue.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Lesson4Leon
+{
+    public record CachedValue<T>
+    {
+        public T? Value { get; set; }
+        public DateTime CreationTime { get; set; }
+        public int Timeout { get; set; }
+    }
+}
---
+++ Lesson4Leon/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson4Leon
+{
+    internal class SimpleGenericCache<T>
+    {
+        private readonly Dictionary<string, CachedValue<T?>> _cache = new();
+
+        internal void Store(string key, T value, int timeout=30)
+        {
+            var cachedValue = new CachedValue<T?> { Value = value, CreationTime = DateTime.Now, Timeout=timeout };
+            _cache[key] = cachedValue;
+        }
+
+        internal T? Fetch(string key)
+        {
+            if (_cache.TryGetValue(key, out var value))
+            {
+                return value;
+            }
+
+            return default;
+        }
+    }
+
+}
