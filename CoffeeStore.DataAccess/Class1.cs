fc921d1bb593a2faedefa7b964a93955c9782b28
Леонид Викторович Гулевский <leonid.gulevskiy@indusoft.ru>
2022-08-26 19:29:07 +0300

    1

--- CoffeeStore.DataAccess/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CoffeeStore.DataAccess
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CoffeeStore.DataAccess/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeStore.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeStore.DataAccess
+{
+    internal interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+    }
+}
