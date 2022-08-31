ba1a4f26384823d942d59e9fb2235521bb4a4aa6
Леонид Викторович Гулевский <leonid.gulevskiy@indusoft.ru>
2022-08-31 12:23:11 +0300

    2

---
+++ Lesson8Leon/Lesson8LeonObserver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson8LeonObserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
